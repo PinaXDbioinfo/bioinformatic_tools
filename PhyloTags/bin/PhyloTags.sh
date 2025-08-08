b7e5325eacee8459052ef23a63c5c47d32f8ffa4
José Emilio Ramírez Piña <emiliopina555@gmail.com>
2025-08-08 10:50:56 -0600

    Add files via upload

---
+++ PhyloTags/bin/PhyloTags.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+tree=$1
+
+mkdir ../Results/Descendants
+
+chmod +x PhyloTags.R
+Rscript PhyloTags.R "${tree}"
+
+mv Node* ../Results/Descendants
+mv labeled_tree.nwk ../Results
+mv labeled_tree.png ../Results
